a755488e011d9062542a324f7a34b7f7b78b6877
basalaev <basalaev.dev@gmail.com>
2021-02-28 14:14:12 +0300

    Fix dependencies

--- Packages/AuthUserStory/Package.swift
+++ Packages/AuthUserStory/Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AuthUserStory",
-            dependencies: ["UIComponents"]
+            dependencies: ["UIComponents"],
             resources: [
                 .process("Resources")
             ]
